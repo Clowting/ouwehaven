0727d9b5ee19364fdc1f66a31c8482eea7e0b669
Thijs Clowting <thijsclowting@hotmail.nl>
2015-04-15 23:32:50 +0200

    Added webportal signup.

--- webportal/includes/head.php
+++ webportal/includes/head.php
@@ -10,6 +10,7 @@
 
     <!-- Custom CSS -->
     <link href="css/style.css" rel="stylesheet">
+    <link href='css/progression.min.css' rel='stylesheet' type='text/css'>
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -17,3 +18,7 @@
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/progression.min.js"></script>
--- webportal/signup.php
+++ webportal/signup.php
@@ -21,6 +21,8 @@
 
     ?>
 
+    <script type="text/javascript" src="js/signup.js"></script>
+
     <title>De 'n ouwe haven - Webportal</title>
 
 </head>
@@ -35,23 +37,84 @@
 
     	<hr/>
 
-    	<div class="row">
-    		<form role="form">
+        <?php
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+            if( isset($_POST['voornaam']) &&
+                isset($_POST['achternaam']) &&
+                isset($_POST['adres']) &&
+                isset($_POST['postcode']) &&
+                isset($_POST['woonplaats'])) {
+
+                $voornaam = strip_tags($_POST['voornaam']);
+                $achternaam = strip_tags($_POST['achternaam']);
+                $adres = strip_tags($_POST['adres']);
+                $postcode = strip_tags($_POST['postcode']);
+                $woonplaats = strip_tags($_POST['woonplaats']);
+
+                if(isset($_POST['tussenvoegsel']) && !empty($_POST['tussenvoegsel'])) {
+                    $tussenvoegsel = strip_tags($_POST['tussenvoegsel']);
+                    $sql = "INSERT INTO oh_members (User_ID, Voornaam, Tussenvoegsel, Achternaam, Adres, Postcode, Woonplaats)
+                            VALUES ($userID, '$voornaam', '$tussenvoegsel', '$achternaam', '$adres', '$postcode', '$woonplaats')";
+
+                } else {
+                    $sql = "INSERT INTO oh_members (User_ID, Voornaam, Achternaam, Adres, Postcode, Woonplaats)
+                            VALUES ($userID, '$voornaam', '$achternaam', '$adres', '$postcode', '$woonplaats')";
+                }
+
+                $insert = $mysqli->query($sql);
+
+                if($insert) {
+                    echo '<div class="alert alert-success" role="alert">Bedankt voor het aanvullen van de gegevens, ze zijn succesvol verwerkt!</div>';
+                    echo '<p>Klik <a href="/webportal">hier</a> om verder te gaan.</p>';
+                } else {
+                    echo '<div class="alert alert-danger" role="alert">Het lijkt er op alsof er een fout is met de verbinding van de database...</div>';
+                    echo '<p>Klik <a href="signup.php">hier</a> om het opnieuw te proberen.</p>';
+                }
+
+            } else {
+                echo '<div class="alert alert-danger" role="alert">Het lijkt er op alsof niet alle gegevens zijn ingevuld...</div>';
+                echo '<p>Klik <a href="signup.php">hier</a> om het opnieuw te proberen.</p>';
+            }
+
+
+        } else {
+        ?>
+
+    	<div class="row col-md-9 col-xs-7">
+    		<form id="signupForm" role="form" method="POST">
     			<div class="form-group">
-    			<label for="voornaam">Voornaam:</label>
-    				<input type="text" class="form-control" id="voornaam">
+                    <label for="voornaam">Voornaam:</label>
+    				<input type="text" class="form-control" name="voornaam" required data-progression="" data-helper="Vul hier uw voornaam in.">
     			</div>
     			<div class="form-group">
     				<label for="tussenvoegsel">Tussenvoegsel:</label>
-    				<input type="text" class="form-control" id="tussenvoegsel">
+    				<input type="text" class="form-control" name="tussenvoegsel" data-progression="" data-helper="Vul hier uw tussenvoegsel in indien u die heeft.">
     			</div>
     			<div class="form-group">
     				<label for="achternaam">Achternaam:</label>
-    				<input type="text" class="form-control" id="achternaam">
+    				<input type="text" class="form-control" name="achternaam" required data-progression="" data-helper="Vul hier uw achternaam in.">
     			</div>
-    			<button type="submit" class="btn btn-default">Verzenden</button>
+    			<div class="form-group">
+    				<label for="adres">Adres:</label>
+    				<input type="text" class="form-control" name="adres" required data-progression="" data-helper="Vul hier uw straat en huisnummer in.">
+    			</div>
+    			<div class="form-group">
+    				<label for="postcode">Postcode:</label>
+    				<input type="text" class="form-control" name="postcode" required maxlength="7" data-progression="" data-helper="Vul hier uw postcode in.">
+    			</div>
+    			<div class="form-group">
+    				<label for="woonplaats">Woonplaats:</label>
+    				<input type="text" class="form-control" name="woonplaats" required data-progression="" data-helper="Vul hier uw woonplaats in.">
+    			</div>
+    			<button type="submit" class="btn btn-default">Verzenden
+    			</button>
     		</form>
     	</div>
+
+        <?php
+        }
+        ?>
 
     </div>
 
