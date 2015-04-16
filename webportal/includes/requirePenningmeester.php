8fb3cc08214d053e638afca8bdc80ca33bd3d8bc
raymondelooff <raymondelooff@ip244-119.stu-wireless.HZeeland.nl>
2015-04-16 09:29:19 +0200

    Fixed permissions

---
+++ webportal/includes/requirePenningmeester.php
@@ -0,0 +1,9 @@
+<?php
+
+	require_once 'requireSession.php';
+	require_once 'functions.php';
+
+	// $roles is defined in 'requireSession.php'
+	if(!isPenningmeester($roles)) {
+		header('Location: /webportal');
+	}
