d6bd3136cf8cb71e40094a2aaffcf7454ee1ed82
Thijs Clowting <thijsclowting@hotmail.nl>
2015-04-21 21:35:02 +0200

    Added image upload.

--- webportal/ships-add.php
+++ webportal/ships-add.php
@@ -48,7 +48,68 @@
                             <li role="presentation"><a href="#">Zoek schip</a></li>
                         </ul>
 
-                        <form id="addShipForm" role="form" method="POST">
+                        <?php
+                        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+                            $naam = cleanInput($_POST['naam']);
+                            $lengte = cleanInput($_POST['lengte']);
+                            $ligplaats = cleanInput($_POST['ligplaats']);
+                            $imgURL = "";
+
+                            if(isset($_FILES['afbeelding']['tmp_name']) && !empty($_FILES['afbeelding']['tmp_name'])) {
+                                $tmp_name = $_FILES['afbeelding']['tmp_name'];
+                                $name = $_FILES['afbeelding']['name'];
+
+                                $allowedImageTypes = array(IMAGETYPE_PNG, IMAGETYPE_JPEG, IMAGETYPE_GIF);
+                                $detectedImageType = exif_imagetype($tmp_name);
+
+                                $shipsDir = "/webportal/images/ships";
+                                $ext = pathinfo($name, PATHINFO_EXTENSION);
+                                $imageName = sha1($naam . $memberID . time()) . '.' . $ext;
+                                $fileUploaded = move_uploaded_file($tmp_name, $_SERVER['DOCUMENT_ROOT'] . "$shipsDir/$imageName");
+
+                                if($fileUploaded) {
+ 
+                                    $imgURL = "$shipsDir/$imageName";
+
+                                }
+
+                            }
+
+
+                            if(in_array($detectedImageType, $allowedImageTypes) || $imgURL == "") {
+
+                                if( validateInput($naam, 2, 128) &&
+                                    validateInput($lengte, 1, 16) &&
+                                    validateInput($ligplaats, 1, 11)) {
+
+                                    $sql = "INSERT INTO oh_ships (Ligplaats_ID, Naam, Lengte, ImgURL)
+                                            VALUES ($ligplaats, '$naam', '$lengte', '$imgURL')";
+
+                                    $insert = $mysqli->query($sql);
+
+                                    if($insert) {
+                                        echo '<div class="alert alert-success" role="alert">Het schip is succesvol toegevoegd!</div>';
+                                        echo '<p>Klik <a href="ships.php">hier</a> om verder te gaan.</p>';
+                                    } else {
+                                        echo '<div class="alert alert-danger" role="alert">Het lijkt er op alsof er een fout is met de verbinding van de database...</div>';
+                                        echo '<p>Klik <a href="ships-add.php">hier</a> om het opnieuw te proberen.</p>';
+                                    }
+
+                                } else {
+                                    echo '<div class="alert alert-danger" role="alert">Het lijkt er op alsof niet alle gegevens zijn ingevuld...</div>';
+                                    echo '<p>Klik <a href="ships-add.php">hier</a> om het opnieuw te proberen.</p>';
+                                }
+
+                            } else {
+                                echo '<div class="alert alert-danger" role="alert">De afbeelding die je probeert up te loaden is geen geldig type. PNG, JPEG en GIF zijn geldig.</div>';
+                                echo '<p>Klik <a href="ships-add.php">hier</a> om het opnieuw te proberen.</p>';
+                            }
+
+
+                        } else {
+                        ?>
+                        <form id="addShipForm" role="form" method="POST" enctype="multipart/form-data">
                             <div class="form-group">
                                 <label for="naam">Naam schip:</label>
                                 <input type="text" class="form-control" name="naam">
@@ -78,7 +139,9 @@
                             <button type="submit" class="btn btn-default">Verzenden
                             </button>
                         </form>
-
+                        <?php
+                        }
+                        ?>
                     </div>
                 </div>
             </div>
