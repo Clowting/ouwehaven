26d93056d2fac9b3d572133923fb864268bb6bab
slennie12 <bout0036@hz.nl>
2015-04-29 22:53:51 +0200

    trying to make dynamic table in the wordpress page, added a jquery function to try to make this happen

--- wp-content/themes/twentyfifteen/functions.php
+++ wp-content/themes/twentyfifteen/functions.php
@@ -353,3 +353,15 @@ require get_template_directory() . '/inc/template-tags.php';
  * @since Twenty Fifteen 1.0
  */
 require get_template_directory() . '/inc/customizer.php';
+
+// /**
+//  * Adds a custom js function :D
+//  */
+// add_action( 'wp_enqueue_scripts', 'add_mooring_js' );
+// function add_mooring_js() {
+// 	wp_enqueue_script(
+// 			'functions_mooring', // name your script so that you can attach other scripts and de-register, etc.
+// 			get_template_directory_uri() . '/js/functions-mooring.js', // this is the location of your script file
+// 			array('jquery') // this array lists the scripts upon which your script depends
+// 	);
+// }
