186ae547dba8a351881dbc6212b4e94417953685
Thijs Clowting <thijsclowting@hotmail.nl>
2015-05-26 22:18:42 +0200

    Fixed invoices page.

--- webportal/invoices.php
+++ webportal/invoices.php
@@ -18,25 +18,20 @@ require_once 'includes/connectdb.php';
         $(document).ready(function () {
 
             $('#addInvoice #add').click(function () {
-                var n = $('div#selectInvoiceLine').length + 1;
-
-                if (10 < n) {
-                    alert('Stop met spelen, terug aan het werk!');
-                    return false;
-                }
-                var selectBox = $('<div class="form-group col-md-7" id="selectInvoiceLine"><label for="invoiceLine' + n + '">Extra Factuurregel <span class="number">' + n + '</span></label> <select class="form-control" name="invoiceLine[]" value="" id="invoiceLine' + n + '">	<?php $sql = 'SELECT * FROM oh_price_category';$result = $dataManager->rawQuery($sql);foreach($result as $res){echo '<option value="'.$res['ID'].'">'.$res['Naam'].'</option>';}?></select> <a href="#" class="remove-box">Remove</a></div>');
+                var selectBox = '<tr> <td class="form-group"> <select class="form-control" name="invoiceLines[]" class="form-control"> <?php $categories = $dataManager->get('oh_price_category'); foreach ($categories as $category) { echo '<option value="' . $category['ID'] . '" id="Categorie_ID">' . $category['Naam'] . '</option>'; } ?> </select> </td> <td class="form-group"> <input type="number" class="form-control" name="invoiceAmounts[]"> </td> <td> <input type="text" class="form-control" name="invoicePrices[]"> </td> <td> <i class="fa fa-minus-circle remove-line"></i> </td> </tr>';
+                selectBox = $(selectBox);
 
                 selectBox.hide();
-                $('#addInvoice div#selectInvoiceLine:last').after(selectBox);
+                $('#invoices tr:last').after(selectBox);
 
                 selectBox.fadeIn('slow');
                 return false;
             });
 
 
-            $('#addInvoice').on('click', '.remove-box', function () {
+            $('#addInvoice').on('click', '.remove-line', function () {
 
-                $(this).parent().fadeOut("slow", function () {
+                $(this).parent().parent().fadeOut("slow", function () {
                     $(this).remove();
                     $('.number').each(function (index) {
                         $(this).text(index + 1);
@@ -69,7 +64,7 @@ include_once 'includes/sidebar.php';
             <div class="col-lg-12">
                 <div class="page-header">
                     <h1>
-                        Facturen</small>
+                        Facturen
                     </h1>
                 </div>
                 <p>Op deze pagina kunt u een factuur aanmaken voor de leden.</p>
@@ -79,110 +74,147 @@ include_once 'includes/sidebar.php';
 
                     $newDate = DateTime::createFromFormat('d/m/Y', $_POST ['date']);
 
-                    $member = cleanInput($_POST ['member']);
                     $date = $newDate->format('Y-m-d');
-                    $lines = serialize($_POST['invoiceLines']);
+                    $invoiceCategories = $_POST['invoiceLines'];
+                    $invoiceAmounts = $_POST['invoiceAmounts'];
+                    $invoicePrices = $_POST['invoicePrices'];
 
-                    if (validateInput($member, 2, 64) && validateDate($date, 'Y-m-d')) {
+                    if (validateDate($date, 'Y-m-d')) {
 
                         $data = array(
-                            'Lid_ID' => $member,
+                            'Lid_ID' => $memberID,
                             'Datum' => $date,
                         );
 
                         $insert = $dataManager->insert('oh_invoices', $data);
 
+                        $factuurID = $dataManager->getInsertId();
+                        $successCount = 0;
+                        $failCount = 0;
+
+                        foreach($invoiceCategories as $key => $category) {
+                            $data = array(
+                                'Factuur_ID' => $factuurID,
+                                'Categorie_ID' => $category,
+                                'Aantal' => $invoiceAmounts[$key],
+                                'Bedrag' => $invoicePrices[$key]
+                            );
+
+                            $insertLine = $dataManager->insert('oh_invoices_line', $data);
+
+                            if($insertLine) {
+                                $successCount++;
+                            } else {
+
+                            }
+                        }
+
                         if ($insert) {
+                            echo '<div class="alert alert-success" role="alert">De factuur is succesvol toegevoegd!</div>';
 
-                            $getID = 'SELECT ID FROM oh_invoices WHERE Lid_ID = ' . $member . ' AND Datum = ' . $date . '';
-                            var_dump($getID);
-
-                            $factuur = $dataManager->rawQuery($getID);
-
-                            if ($factuur != null) {
-                                $insert2 = 'INSERT INTO oh_invoices_line(Factuur_ID, Categorie_ID, Aantal, Bedrag) VALUES($factuur, )';
+                            if($successCount > 0) {
+                                echo '<div class="alert alert-success" role="alert"><strong>' . $successCount . ' factuurregels</strong> succesvol toegevoegd!</div>';
                             }
 
-                            echo '<div class="alert alert-success" role="alert">Bedankt voor het aanvullen van de gegevens, ze zijn succesvol verwerkt!</div>';
+                            if($failCount > 0) {
+                                echo '<div class="alert alert-danger" role="alert"><strong>' . $failCount . ' factuurregels</strong> konden niet worden toegevoegd.</div>';
+                            }
+
                             echo '<p>Klik <a href="/webportal">hier</a> om naar de hoofdpagina te gaan.</p>';
                             echo "<p>Of klik <a href=" . $_SERVER ['REQUEST_URI'] . ">hier</a> om nog een factuur toe te voegen.";
-
-
                         } else {
                             echo '<div class="alert alert-danger" role="alert">Het lijkt er op alsof er een fout is met de verbinding van de database...</div>';
                             echo "<p>Klik <a href=" . $_SERVER ['REQUEST_URI'] . ">hier</a> om het opnieuw te proberen.</p>";
                         }
                     } else {
                         echo '<div class="alert alert-danger" role="alert">Het lijkt er op alsof niet alle gegevens zijn ingevuld...</div>';
-                        var_dump($data);
                         echo "<p>Klik <a href=" . $_SERVER ['REQUEST_URI'] . ">hier</a> om het opnieuw te proberen.</p>";
                     }
                 } else {
+                    ?>
+
+                    <form class="clearfix horizontalSearchForm" id="addInvoice" role="form" method="POST"
+                          enctype="multipart/form-data">
+
+                        <div class="form-group col-md-5" id="selectMembers">
+                            <label for="selectMember">Selecteer lid</label>
+
+                            <select class="form-control" name="selectMember" id="selectMember">
+                                <?php
+                                $members = $dataManager->get('oh_members');
+
+                                foreach ($members as $member) {
+                                    $eigenaar = generateName($member['Voornaam'], $member['Tussenvoegsel'], $member['Achternaam']);
+
+                                    echo '<option value="' . $member["ID"] . '">' . $eigenaar . '</option>';
+                                }
+                                ?>
+                            </select>
+                        </div>
+
+                        <div class="form-group col-md-5">
+                            <label for="date">Factuurdatum:</label>
+                            <input type="text" value="<?php echo date("d/m/Y") ?>" class="form-control formDate"
+                                   name="date"
+                                   id="date">
+                        </div>
+
+                        <div class="form-group col-md-1" id="add">
+                            <button type="button" class="btn btn-default " name="add" id="add">Voeg extra regel toe
+                            </button>
+                        </div>
+
+                        <div class="table-responsive col-md-12">
+                            <table class="table table-striped" id="invoices">
+                                <thead>
+                                    <tr>
+                                        <th>Categorie</th>
+                                        <th>Aantal</th>
+                                        <th>Prijs</th>
+                                        <th>Actie</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td class="form-group">
+                                            <select class="form-control" name="invoiceLines[]" class="form-control">
+                                                <?php
+                                                    $categories = $dataManager->get('oh_price_category');
+
+                                                    foreach ($categories as $category) {
+                                                        echo '<option value="' . $category['ID'] . '" id="Categorie_ID">' . $category['Naam'] . '</option>';
+                                                    }
+                                                ?>
+                                            </select>
+                                        </td>
+                                        <td class="form-group">
+                                            <input type="number" class="form-control" name="invoiceAmounts[]">
+                                        </td>
+                                        <td>
+                                            <input type="text" class="form-control" name="invoicePrices[]">
+                                        </td>
+                                        <td>
+                                            <i class="fa fa-minus-circle remove-line"></i>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div class="col-md-1">
+                            <button type="submit" class="btn btn-default ">Aanmaken</button>
+                        </div>
+
+                    </form>
+
+                <?php
+                }
                 ?>
 
-                <form class="clearfix horizontalSearchForm" id="addInvoice" role="form" method="POST"
-                      enctype="multipart/form-data">
-
-                    <div class="form-group col-md-5" id="selectMembers">
-                        <label for="selectMember">Selecteer lid</label>
-
-                        <select class="form-control" name="selectMember" id="selectMember">
-                            <?php
-                            $members = $dataManager->get('oh_members');
-
-                            foreach($members as $member) {
-                                $eigenaar = generateName($member['Voornaam'], $member['Tussenvoegsel'], $member['Achternaam']);
-
-                                echo '<option value="' . $member["ID"] . '">' . $eigenaar . '</option>';
-                            }
-                            ?>
-                        </select>
-                    </div>
-
-                    <div class="form-group col-md-5">
-                        <label for="date">Factuurdatum:</label>
-                        <input type="text" value="<?php echo date("d/m/Y") ?>" class="form-control formDate" name="date"
-                               id="date">
-                    </div>
-                    
-                    <div class="form-group col-md1 id=" add
-                    ">
-                    <button type="button" class="btn btn-default " name="add" id="add">Voeg extra regel toe</button>
-            </div>
-
-            <div class="form-group col-md-7" id="selectInvoiceLine">
-                <label for="invoiceLine1">FactuurRegel 1</label>
-
-                <select class="form-control" name="invoiceLines[]" id="invoiceLine1" class="form-control">
-                    <?php
-                    $sql = 'SELECT ID, Naam FROM oh_price_category';
-                    $result = $dataManager->rawQuery($sql);
-
-                    foreach ($result as $res) {
-                        echo '<option value="' . $res['ID'] . '" id="Categorie_ID">' . $res['Naam'] . '</option>';
-                    }
-                    ?>
-                </select>
-
+                <hr/>
 
             </div>
-            <div class="col-md-2">
-
-                <button type="submit" class="btn btn-default " name="submit" value="submit" id="submit">Opslaan</button>
-            </div>
-
-
-            </form>
-
-            <?php
-            }
-            ?>
-
-            <hr/>
-
         </div>
     </div>
-</div>
 </div>
 
 <!-- /#page-content-wrapper -->
