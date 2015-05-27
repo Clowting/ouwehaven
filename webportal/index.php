f02d8c62485cd37ce6e48f0af7b54b12acd0d666
Raymon de Looff <raydelooff@gmail.com>
2015-05-27 17:58:54 +0200

    New webportal index page

--- webportal/index.php
+++ webportal/index.php
@@ -34,9 +34,12 @@
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-lg-12">
-                        <h1>De 'n Ouwe Haven</h1>
-                        <p>This template has a responsive menu toggling system. The menu will appear collapsed on smaller screens, and will appear non-collapsed on larger screens. When toggled using the button below, the menu will appear/disappear. On small screens, the page content will be pushed off canvas.</p>
-                        <p>Make sure to keep all page content within the <code>#page-content-wrapper</code>.</p>
+                        <h1>Welkom De 'n Ouwe Haven</h1>
+                        <p>
+                            Op dit portaal kunt u onder andere uw persoonlijke gegevens wijzigen, schepen toevoegen en havenplaatsen reserveren.<br>
+                            Daarnaast kunt u hier al uw betalingen en facturen terugvinden.
+                        </p>
+                        <img src="images/zee.jpg" alt="Zee" />
                     </div>
                 </div>
             </div>
