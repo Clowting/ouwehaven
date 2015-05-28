1eefe022697057469fe10c4a5d636898e7b5df76
Thijs Clowting <thijs@clowting.me>
2015-05-28 09:44:50 +0200

    Removed unused <?php ?>

--- webportal/cashbook.php
+++ webportal/cashbook.php
@@ -17,7 +17,7 @@ require_once 'includes/connectdb.php';
     ?>
 
     <title><?php echo SITE_TITLE; ?> - Kasboek </title>
-    
+
 </head>
 
 <body>
@@ -38,34 +38,37 @@ include_once 'includes/sidebar.php';
         <div class="row">
             <div class="col-lg-12">
                 <div class="page-header">
-                    <h1>Kasboek <small>Zoeken</small></h1>
-                    
-                    
+                    <h1>Kasboek
+                        <small>Zoeken</small>
+                    </h1>
+
+
                 </div>
 
-                <p>Op deze pagina kunt u gegevens in het kasboek zetten, deze worden direct opgeslagen wanneer u op volgende drukt</p>
-                <p>Wanneer u meerdere kasboek gegevens wilt invoeren, kunt u kiezen voor "nog 1 toevoegen, wanneer u klaar bent kunt u weer op volgende drukken om verder te gaan</p>
-                                
-                    <ul class="nav nav-tabs">
-                        <li role="presentation" class="active"><a href="cashbook.php">Alle Kasboektransacties</a></li>
-                        <li role="presentation"><a href="cashbook-add.php">Voeg toe aan Kasboek</a></li>
-                        
-                    </ul>
-                     <?php
+                <p>Op deze pagina kunt u gegevens in het kasboek zetten, deze worden direct opgeslagen wanneer u op
+                    volgende drukt</p>
 
-        		?>
-                <form class="clearfix horizontalSearchForm" id="searchCashBook" role="form" method="POST" enctype="multipart/form-data">
-                    
-                    
+                <p>Wanneer u meerdere kasboek gegevens wilt invoeren, kunt u kiezen voor "nog 1 toevoegen, wanneer u
+                    klaar bent kunt u weer op volgende drukken om verder te gaan</p>
+
+                <ul class="nav nav-tabs">
+                    <li role="presentation" class="active"><a href="cashbook.php">Alle Kasboektransacties</a></li>
+                    <li role="presentation"><a href="cashbook-add.php">Voeg toe aan Kasboek</a></li>
+
+                </ul>
+                <form class="clearfix horizontalSearchForm" id="searchCashBook" role="form" method="POST"
+                      enctype="multipart/form-data">
+
+
                     <div class="form-group col-md-8">
                         <label for="desc">Beschrijving:</label>
                         <input type="text" class="form-control" name="desc" id="desc">
                     </div>
-                   
+
                     <div class="form-group col-md-2">
                         <label for="code">Debet/Credit:</label>
                         <select class="form-control" name="code" id="code">
-                            <option value=""> </option>
+                            <option value=""></option>
                             <option value="D">Debet</option>
                             <option value="C">Credit</option>
                         </select>
@@ -75,13 +78,13 @@ include_once 'includes/sidebar.php';
                         <label for="date">Datum uitgevoerd:</label>
                         <input type="text" value="" class="form-control formDate" name="date" id="date">
                     </div>
-                    
-                    
+
+
                     <div class="form-group col-md-5">
                         <label for="sender">Van:</label>
                         <input type="text" class="form-control" name="sender" id="sender">
                     </div>
-                    
+
                     <!-- Misschien moet van en voor een dropdown menu worden waar eventueel iets aan kan worden toegevoegd, dit om verschillende namen tegen te gaan -->
 
                     <div class="form-group col-md-5">
@@ -93,15 +96,15 @@ include_once 'includes/sidebar.php';
                         <button type="submit" class="btn btn-primary" id="search">Zoeken</button>
                     </div>
                 </form>
-                <?php 
-                        
+                <?php
+
                 ?>
 
                 <hr/>
-                
+
                 <div class="table-responsive">
-                	<form id="toPDFForm" method="POST" action="pdf-creator/cashbookPDF.php">
-                    <table class="table table-striped table-hover" id="cashbookEntriesTable">
+                    <form id="toPDFForm" method="POST" action="pdf-creator/cashbookPDF.php">
+                        <table class="table table-striped table-hover" id="cashbookEntriesTable">
 
                             <thead>
                             <tr>
@@ -111,26 +114,27 @@ include_once 'includes/sidebar.php';
                                 <th>Code</th>
                                 <th>Van</th>
                                 <th>Voor</th>
-                                <th width="5%"><button type="submit" class="btn btn-primary">Maken PDF</button></th>
-                                
+                                <th width="5%">
+                                    <button type="submit" class="btn btn-primary">Maken PDF</button>
+                                </th>
+
                             </tr>
                             </thead>
-                            
+
                             <tbody id="cashbookEntries">
-                            
 
 
                             </tbody>
-                	</table>
-                	</form>
+                        </table>
+                    </form>
                 </div>
-                    <div class="text-center" id="page-selection">
-                        <ul id="pagination"></ul>
-                    </div>
+                <div class="text-center" id="page-selection">
+                    <ul id="pagination"></ul>
                 </div>
             </div>
         </div>
     </div>
+</div>
 
 <!-- /#page-content-wrapper -->
 
@@ -138,7 +142,7 @@ include_once 'includes/sidebar.php';
 <!-- /#wrapper -->
 
 <!-- Footer -->
-        <script src="js/cashbook-pagination.js"></script>
+<script src="js/cashbook-pagination.js"></script>
 <?php
 
 include_once 'includes/footer.php';
